08a520a650e2ed29035d74d7e11c23fa0fc37c4a
feelmyears <philipmeyers2017@u.northwestern.edu>
2018-05-12 18:38:42 -0500

    Ran on shortlist of websites, added repeat flat

--- ifc_tester.py
+++ ifc_tester.py
@@ -11,10 +11,14 @@ def read_sites(path):
 	with open(path, 'r') as f:
 		return [url.strip() for url in f.readlines()]
 
-def start_chrome(chrome_path, proxy_port, remote_debugging_port):
+def start_chrome(chrome_path, proxy_port, remote_debugging_port, enable_quic=True):
 #	cmd = f'/Applications/Google\\ Chrome.app/Contents/MacOS/Google\\ Chrome --user-data-dir=/tmp/chrome  --proxy-server="localhost:8080"  --enable-quic --remote-debugging-port={remote_debugging_port} --headless  --proxy-server="quic://rodrigo918.hopto.org:443"'
 	# cmd = f'/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --user-data-dir=/tmp/chrome --enable-quic --proxy-server="127.0.0.1:18443" www.google.com'
-	cmd = f'{chrome_path} --user-data-dir=/tmp/chrome  --headless --enable-quic --proxy-server="localhost:{proxy_port}" --remote-debugging-port={remote_debugging_port}'
+	quic_flag = ""
+	if enable_quic:
+		quic_flag = '--enable-quic'
+
+	cmd = f'{chrome_path} --user-data-dir=/tmp/chrome  --headless {enable_quic} --proxy-server="localhost:{proxy_port}" --remote-debugging-port={remote_debugging_port}'
 	p  = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
 #	p.communicate()
 	return p
@@ -34,6 +38,7 @@ if __name__ == "__main__":
 	parser.add_argument('--sites', '-s', help="Path to txt file of sites", type=str)
 	parser.add_argument('--chrome', '-c', help="Path to Chrome installation", type=str)
 	parser.add_argument('--output', '-o', help="Dir to save har files", type=str)
+	parser.add_argument('--repeat', '-r', help="Dir to save har files", type=int, default=5)
 	args = parser.parse_args()
 
 	# output_dir = args.output
@@ -43,20 +48,21 @@ if __name__ == "__main__":
 	test_urls = read_sites(args.sites)
 	test_protocols = [('quic', 18443), ('tcp', 18080)]
 	# test_protocols = [('tcp', 18080)]
-	for protocol, port in test_protocols:
-		for i, url in enumerate(test_urls):
-			hostname_parts = urlparse(url).hostname.split('.')
-			hostname = None
-			if len(hostname_parts) > 2:
-				hostname = hostname_parts[1]
-			else:
-				hostname = hostname_parts[0]
-			
-			p_browser = start_chrome(args.chrome, port, 9222)
-			sleep(3)
-			print(f"Capturing HAR for {hostname} using protocol {protocol}")
-			p_har = capture_har(os.path.join(output_dir, f'{i}_{hostname}_{protocol}.har'), url)
-			p_har.wait()
+	for i, url in enumerate(test_urls):
+		for r in range(1, args.repeat+1):
+			for protocol, port in test_protocols:
+				hostname_parts = urlparse(url).hostname.split('.')
+				hostname = None
+				if len(hostname_parts) > 2:
+					hostname = hostname_parts[1]
+				else:
+					hostname = hostname_parts[0]
+				
+				p_browser = start_chrome(args.chrome, port, 9222)
+				sleep(3)
+				print(f"Capturing HAR for {hostname} using protocol {protocol}")
+				p_har = capture_har(os.path.join(output_dir, f'{r}_{hostname}_{protocol}.har'), url)
+				p_har.wait()
 
-			p_browser.kill()
-			sleep(5)
+				p_browser.kill()
+				sleep(5)
